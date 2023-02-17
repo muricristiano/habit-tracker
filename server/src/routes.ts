1b54732c09e7fa754087f27ae8de1d9a08f1267b
Murilo Ribeiro Cristiano <muricristiano@gmail.com>
2023-02-17 18:09:24 -0300

    front web+mobile advanced + route to toggle habit

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -77,6 +77,58 @@ export async function appRoutes(app: FastifyInstance) {
             completedHabits
         }
     })
+
+    // Habit Toggle
+    app.patch('/habits/:id/toggle', async (request) => {
+        // route param (parameter of identification)
+        const toggleHabitParams = z.object({
+            id: z.string().uuid()
+        })
+
+        const { id } = toggleHabitParams.parse(request.params)
+
+        const today = dayjs().startOf('day').toDate()
+
+        let day = await prisma.day.findUnique({
+            where: {
+                date: today
+            }
+        })
+
+        if (!day) {
+            day = await prisma.day.create({
+                data: {
+                    date: today
+                }
+            })
+        }
+
+        const dayHabit = await prisma.dayHabit.findUnique({
+            where: {
+                day_id_habit_id: {
+                    day_id: day.id,
+                    habit_id: id,
+                }
+            }
+        })
+
+        if (dayHabit) {
+            // If Completed the habit > toggle to cancel
+            await prisma.dayHabit.delete({
+                where: {
+                    id: dayHabit.id
+                }
+            })
+        } else {
+            // If not completed > complete the habit on the day
+        await prisma.dayHabit.create({
+            data: {
+                day_id: day.id,
+                habit_id: id
+            }
+        })
+        }
+    })
 }
 
 
